f3006814ba382369862c8619bc55a0118d35009d
Prifton <MLIWAPRIFTON@GMAIL.COM>
2025-08-12 09:42:48 +0300

    Add files via upload

---
+++ assignmentmy_list.py
@@ -0,0 +1,27 @@
+# Create an empty list called my_list
+my_list = []
+
+# Append elements: 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert the value 15 at the second position (index 1)
+my_list.insert(1, 15)
+
+# Extend my_list with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
+
+# Print the final list
+print("Final list:", my_list)
